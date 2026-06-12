35fd6192625ef14140cc3ec7c63118122cd9395a
Kamesh Hedau <kameshhedau19@gmail.com>
2026-06-13 01:40:48 +0530

    update

--- src/components/EventsSection.jsx
+++ src/components/EventsSection.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { FaFire, FaMusic, FaHorse, FaRing, FaPalette } from "react-icons/fa";
+import { FaMusic, FaHorse, FaRing, FaPalette } from "react-icons/fa";
 
 const events = [
   {
